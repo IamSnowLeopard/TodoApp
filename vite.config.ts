42f8e51d1b118f3491bd349ceace7717a73bef6a
IamSnowLeopard <146442423+IamSnowLeopard@users.noreply.github.com>
2024-01-29 20:27:17 -0500

    bug fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-//export default defineConfig({
-//  base: '/<REPO>/'
-//})
-
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        404: resolve(__dirname, "public/404.html"),
-      },
-    },
-  },
-});
+  base: '/<REPO>/'
+})
